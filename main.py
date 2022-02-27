b04679825a6216b453660432c0bda28082e37678
viniciusddrft <viniciusddrft5@gmail.com>
2022-02-27 15:11:10 -0300

    remove .zip

--- main.py
+++ main.py
@@ -6,26 +6,12 @@ app = Flask(__name__)
 data = []
 
 @app.route("/",methods =["GET"])
-def apiGet():
+def apiGetAll():
     return jsonify({
             'statusCode': 200,
             'body':data,
         })
 
-@app.route("/",methods =["POST"])
-def apiPost():
-    result = request.get_json()
-    if(result.get('title')):
-        data.append(result)
-        return jsonify({
-            'statusCode': 200,
-            'body':result,
-        })
-    else:
-        return jsonify({
-            'statusCode': 404,
-            'body':{},
-        })
 
 if(__name__ == '__main__'):
     port = int(os.environ.get("PORT",5000))
